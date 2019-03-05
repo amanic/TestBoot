5a6c82bcb9c239955bb0beff246484764cce8f9b
martea <1642485710@qq.com>
2019-03-05 18:00:30 +0800

    自定义的java.lang.String为什么用不了

---
+++ src/main/java/java/lang/String.java
@@ -0,0 +1,12 @@
+package java.lang;
+
+/**
+ * @auther chen.haitao
+ * @date 2019-03-05
+ */
+public class String {
+
+    public static void main(String[] args) {
+        System.out.println("");
+    }
+}
